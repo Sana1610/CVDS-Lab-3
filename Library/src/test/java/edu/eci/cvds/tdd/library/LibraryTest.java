953c574e0ff9c97fc5103be516c505eeb409df3d
Santiago Naranjo <santiago.naranjo-a@mail.escuelaing.edu.co>
2024-08-31 11:44:42 -0500

    Creacion de clases inicial

---
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,20 @@
+package edu.eci.cvds.tdd.library;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class LibraryTest
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+}
