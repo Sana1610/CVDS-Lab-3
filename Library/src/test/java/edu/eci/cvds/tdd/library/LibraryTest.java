51dee632a3e07827c021448bad8a151212d21fcb
Santiago Naranjo <santiago.naranjo-a@mail.escuelaing.edu.co>
2024-08-31 13:02:16 -0500

    little changes made in librarytest

--- Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -4,17 +4,52 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class LibraryTest
-        extends TestCase
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.loan.Loan;
+import edu.eci.cvds.tdd.library.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LibraryTest extends TestCase
 {
+    private Library library;
+    private Book book;
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public LibraryTest( String testName )
     {
         super( testName );
     }
 
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LibraryTest.class );
+    }
+
+    /**
+     * creates some testing objects
+     * @Before any testing is run
+     */
+    public void before(){
+        library = new Library();
+        book = new Book("title", "author", "isbn");
+    }
+
+    /**
+     * @test
+     * verifies is the book was added correctly into the libray
+     */
+    public void testAddBook (Book book){
+        assertTrue(Library.addBook(book));
+    }
 }
